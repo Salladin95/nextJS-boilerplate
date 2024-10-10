da376dbf4174d155970ec007c0567810c8a8118b
khalid <shkhalid95@mail.ru>
2024-10-10 20:14:06 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "~/src/src/shared";
+
+export default function Home() {
+	return (
+		<main>
+			<h1>
+				HELLO THERE
+			</h1>
+			<Button>Click me</Button>
+		</main>
+	);
+}
